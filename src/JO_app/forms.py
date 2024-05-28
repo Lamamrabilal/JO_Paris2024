863d6023f0faf61f430fb3c515cae9c10a61320d
Bilal Lamamra <bilal.lamamra@yahoo.fr>
2024-05-29 00:52:40 +0200

    Des modifications apporter pour les formulaires

--- src/JO_app/forms.py
+++ src/JO_app/forms.py
@@ -5,33 +5,43 @@ from .models import Utilisateur, Reservation, Ticket, OffreDeBillet
 class UtilisateurCreationForm(UserCreationForm):
     nom = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
         'class': 'form-control my-2',
-        'placeholder': "Your Last Name*"
+        'placeholder': "Nom*"
     }))
     
     prenom = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
         'class': 'form-control my-2',
-        'placeholder': "Your First Name*"
+        'placeholder': "Prénom*"
     }))
     
-    email = forms.EmailField(widget=forms.TextInput(attrs={
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
         'class': 'form-control my-2',
-        'placeholder': "Your Email*"
+        'placeholder': "Email*"
     }))
     
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
+    password1 = forms.CharField(label="Mot de Passe", widget=forms.PasswordInput(attrs={
         'class': 'form-control my-2',
-        'placeholder': "Enter Password*"
+        'placeholder': "Mot de Passe*"
     }))
     
-    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={
+    password2 = forms.CharField(label="Confirmer Mot de Passe", widget=forms.PasswordInput(attrs={
         'class': 'form-control my-2',
-        'placeholder': "Confirm Password*"
+        'placeholder': "Confirmer Mot de Passe*"
     }))
     
     class Meta:
         model = Utilisateur
-        fields = ['nom', 'prenom', 'email', 'password', 'password_confirm']
-   
+        fields = ['nom', 'prenom', 'email', 'password1', 'password2']
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+        
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Les mots de passe ne correspondent pas")
+        
+        return cleaned_data
+
 
 class UtilisateurLoginForm(AuthenticationForm):
     
@@ -68,6 +78,8 @@ class UtilisateurProfileForm(forms.ModelForm):
 
 
 class ReservationForm(forms.ModelForm):
+    nom_utilisateur = forms.CharField(label='Nom', max_length=100)
+    prenom_utilisateur = forms.CharField(label='Prénom', max_length=100)
     class Meta:
         model = Reservation
         fields = ['utilisateur', 'offre_de_billets']  
