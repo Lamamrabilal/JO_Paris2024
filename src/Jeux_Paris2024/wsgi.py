98a118948eef3f809decd45e103a56522e538710
Bilal Lamamra <bilal.lamamra@yahoo.fr>
2024-06-13 00:56:28 +0200

    Actualisation de requirements.txt,settings et wsgi

--- src/Jeux_Paris2024/wsgi.py
+++ src/Jeux_Paris2024/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jeux_Paris2024.settings')
 
-application= get_wsgi_application()
+JO_app= get_wsgi_application()
