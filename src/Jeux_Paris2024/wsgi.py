cbd9113f4acf63678458a0a385b92a9eb0e3fcd9
Bilal Lamamra <bilal.lamamra@yahoo.fr>
2024-06-13 20:20:50 +0200

    mise à jour du fichier wsgi

--- src/Jeux_Paris2024/wsgi.py
+++ src/Jeux_Paris2024/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jeux_Paris2024.settings')
 
-JO_app= get_wsgi_application()
+application= get_wsgi_application()
