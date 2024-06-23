78af8cb7c1f054e5b7d4c56e2266d85e7597a41e
Bilal Lamamra <bilal.lamamra@yahoo.fr>
2024-06-23 13:27:11 +0200

    mise à jour du settings

--- src/Jeux_Paris2024/settings.py
+++ src/Jeux_Paris2024/settings.py
@@ -8,24 +8,22 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Initialise environ
-env = environ.Env(
-    # définir les valeurs par défaut et les forcer à être de type 'str'
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 
 # Lire le fichier .env
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+DJANGO_SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['joparis2024-dc4cb5fdd565.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['joparis2024-233dd76be14d.herokuapp.com', 'localhost']
+
 AUTH_USER_MODEL = 'JO_app.Utilisateur'
 
 # Application definition
@@ -43,13 +41,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Jeux_Paris2024.urls'
@@ -79,9 +77,6 @@ DATABASES = {
     'default': dj_database_url.parse(env('DJANGO_DATABASE_URL'))
 }
 
-# Définition des options pour la base de données par défaut
-DATABASES['default']['OPTIONS'] = {'options': '-c search_path=myschema,public'}
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
@@ -107,25 +102,19 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = env('DJANGO_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('DJANGO_EMAIL_HOST_PASSWORD')
 
-SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
-
-# Internationalization
-LANGUAGE_CODE = 'FR-fr'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # URL de base pour servir les fichiers médias
 MEDIA_URL = '/media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+# Configurations de session
 SESSION_COOKIE_AGE = 1209600  # Deux semaines
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
