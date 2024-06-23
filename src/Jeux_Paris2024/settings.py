1564483188d388910e350da2d534f7463254502f
Bilal Lamamra <bilal.lamamra@yahoo.fr>
2024-06-23 18:45:57 +0200

    mise à jour du fichier settings

--- src/Jeux_Paris2024/settings.py
+++ src/Jeux_Paris2024/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'JO_app',
+    'JO_app.apps.JoAppConfig',
 ]
 
 MIDDLEWARE = [
