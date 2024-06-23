cf7ee23ef5559e65ef29cd7178b09a4a287ae43f
Bilal Lamamra <bilal.lamamra@yahoo.fr>
2024-06-23 20:32:07 +0200

    Changement apporter dans le fichier settings

--- src/Jeux_Paris2024/settings.py
+++ src/Jeux_Paris2024/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'JO_app.apps.JoAppConfig',
+    'JO_app',
 ]
 
 MIDDLEWARE = [
